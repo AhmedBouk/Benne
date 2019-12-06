0c19f8463766dc36fd403eaea3bfa13e7cef1478
Druxys <drudrux@gmail.com>
2019-12-06 20:28:29 +0100

    add Historic entity

---
+++ src/Entity/Historic.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\HistoricRepository")
+ */
+class Historic
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $research;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $type_alert;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $is_enabled;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $updated_at;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getResearch(): ?string
+    {
+        return $this->research;
+    }
+
+    public function setResearch(?string $research): self
+    {
+        $this->research = $research;
+
+        return $this;
+    }
+
+    public function getTypeAlert(): ?string
+    {
+        return $this->type_alert;
+    }
+
+    public function setTypeAlert(?string $type_alert): self
+    {
+        $this->type_alert = $type_alert;
+
+        return $this;
+    }
+
+    public function getIsEnabled(): ?bool
+    {
+        return $this->is_enabled;
+    }
+
+    public function setIsEnabled(bool $is_enabled): self
+    {
+        $this->is_enabled = $is_enabled;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(?\DateTimeInterface $updated_at): self
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+}
