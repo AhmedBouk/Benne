aba0b4ca43a0b993af68e2b05f55b7eb33969b1c
Druxys <drudrux@gmail.com>
2020-01-09 14:15:56 +0100

    change coordinates type on the entity Dumpster

--- src/Controller/BinController.php
+++ src/Controller/BinController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Dumpster;
 
 class BinController extends AbstractController
 {
@@ -12,9 +13,27 @@ class BinController extends AbstractController
      */
     public function index()
     {
+        $entityManager = $this->getDoctrine()->getManager();
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/BinController.php',
         ]);
+    }
+
+    /**
+     * @Route("/add_bin", name="add_bin")
+     */
+    public function addBin() : Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $dumpster = new Dumpster();
+        $dumpster->setName('test');
+        $dumpster->setType('typeTest');
+        $dumpster->setIsEnabled(1);
+        $dumpster->setStatus('testStatus');
+        $dumpster->setCoordinates(['']);
+
+
     }
 }
---
+++ src/Controller/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/user", name="user")
+     */
+    public function index()
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/UserController.php',
+        ]);
+    }
+
+}
