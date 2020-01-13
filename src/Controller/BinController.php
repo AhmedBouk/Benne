de50f00b0abca94a3f454f806a65cc5d629d8635
Nicolas Becuwe <niconzeweb@wanadoo.fr>
2020-01-13 16:36:37 +0100

    add_json_to_database method first draft

--- src/Controller/BinController.php
+++ src/Controller/BinController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Dumpster;
 use Symfony\Component\HttpFoundation\Response;
@@ -37,7 +38,65 @@ class BinController extends AbstractController
         $entityManager->persist($dumpster);
         $entityManager->flush();
         return new Response('Saved new client with id '.$dumpster->getId());
+    }
+
+    /**
+     * @Route("json_to_object", name="json_to_object")
+     */
+    public function JSONtoObject()
+    {
+        $string = file_get_contents('../uploads/benneMontpellier.json');
+
+        $jsonInArray = json_decode($string, true);
+    }
 
 
+
+    /**
+     * @Route("/add_json_to_database", name="add_json_to_database")
+     */
+    public function addJSONtoDatabase():Response
+    {
+        $string = file_get_contents('../uploads/benneMontpellier.json');
+        $jsonInArray = json_decode($string, true);
+        $bins = $jsonInArray['features'];
+
+        $error = array(
+            "type" => 0,
+            "name" => 0,
+            "coordinates" => 0,
+        );
+
+        foreach($bins as $json) {
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $dumpster = new Dumpster();
+
+            if(!empty($json{'properties'}{'type'}) && is_string($json{'properties'}{'type'})){
+                $dumpster->setType($json['properties']['type']);
+            }else{
+                $error['type']++;
+            }
+            if(!empty($json{'properties'}{'rue'}) && is_string($json{'properties'}{'rue'})){
+                $dumpster->setName($json['properties']['rue']);
+            }else{
+                $error['name']++;
+            }
+            if(!empty($json{'geometry'}{'coordinates'}['0']) && !empty($json{'geometry'}{'coordinates'}['0'])){
+                $dumpster->setCoordinates('POINT(' . $json["geometry"]["coordinates"]["0"] . ' ' . $json["geometry"]["coordinates"]["1"] . ')');
+            }else{
+                $error['coordinates']++;
+            }
+            $dumpster->setStatus('testStatus');
+            $dumpster->setIsEnabled(1);
+
+            $em = $dumpster->getCoordinates();
+            print_r($em);
+
+            $entityManager->persist($dumpster);
+            $entityManager->flush();
+
+        }
+        return new JsonResponse($jsonInArray{'features'}, Response::HTTP_CREATED);
     }
 }
