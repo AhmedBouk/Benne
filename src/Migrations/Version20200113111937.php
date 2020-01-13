0c2141f5bf27ff5c1f64cbac49e02c87c20be417
Druxys <drudrux@gmail.com>
2020-01-13 14:13:32 +0100

    add lexik/jwt bundle

--- src/Migrations/Version20200113111937.php
+++ src/Migrations/Version20200113111937.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191218123144 extends AbstractMigration
+final class Version20200113111937 extends AbstractMigration
 {
     public function getDescription() : string
     {
