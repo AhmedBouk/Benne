b3bb6e80a5ceb598d0e37218b80b2c9e391fe4ad
Druxys <drudrux@gmail.com>
2020-01-17 10:40:46 +0100

    creating the SecurityController + update config jwt

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/security", name="security")
+     */
+    public function index()
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/SecurityController.php',
+        ]);
+    }
+
+    /**
+     * @Route(name="api_login", path="/api/login_check")
+     * @return JsonResponse
+     */
+    public function api_login(): JsonResponse
+    {
+        $user = $this->getUser();
+        return new Response([
+            'email' => $user->getEmail(),
+            'roles' => $user->getRoles(),
+        ]);
+    }
+}
