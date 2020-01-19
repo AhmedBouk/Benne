5d089d5c4662ca23e283450d18ee072e556f76b3
Druxys <drudrux@gmail.com>
2020-01-19 16:18:28 +0100

    update security config

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,32 +2,51 @@
 
 namespace App\Controller;
 
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SecurityController extends AbstractController
-{
-    /**
-     * @Route("/security", name="security")
-     */
-    public function index()
+{    /**
+ * @Route(name="api_login", path="/api/login_check")
+ * @return JsonResponse
+ */
+    public function api_login(): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/SecurityController.php',
+        $user = $this->getUser();
+
+        return new Response([
+            'email' => $user->getMail(),
+            'roles' => $user->getRoles(),
         ]);
     }
 
     /**
-     * @Route(name="api_login", path="/api/login_check")
+     * @Route("/login", name="app_login")
+     * @param AuthenticationUtils $authenticationUtils
      * @return JsonResponse
      */
-    public function api_login(): JsonResponse
+    public function login(AuthenticationUtils $authenticationUtils)
     {
-        $user = $this->getUser();
-        return new Response([
-            'email' => $user->getEmail(),
-            'roles' => $user->getRoles(),
-        ]);
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastMail = $authenticationUtils->getLastUsername();
+        return new JsonResponse(['last_mail' => $lastMail, 'error' => $error]);
     }
+
+    /**
+     * @Route("/logout", name="app_logout", methods={"GET"})
+     * @throws Exception
+     */
+    public function logout()
+    {
+        // controller can be blank: it will never be executed!
+        throw new Exception('Don\'t forget to activate logout in security.yaml');
+    }
+
+
 }
